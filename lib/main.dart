66303a4daedcc20f0854c575a91c3a7134131e72
FAHAD ALI <m.mohsin00933@gmail.com>
2024-05-16 14:13:25 -0700

    fahad

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_app/bloc/counter/counter_bloc.dart';
+import 'package:bloc_app/bloc/example_two/example_two_bloc.dart';
 import 'package:bloc_app/views/counter_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,15 +14,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-        create: (context) => CounterBloc(),
-        child: MaterialApp(
-          title: 'Bloc App',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
-          home: const CounterScreen(),
-        ));
+    return MultiBlocProvider(providers: [
+      BlocProvider(create: (_)=>CounterBloc()),
+      BlocProvider(create: (_)=>SwitchBloc())
+
+    ], child: MaterialApp(
+      title: 'Bloc App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CounterScreen(),
+    ));
   }
 }
